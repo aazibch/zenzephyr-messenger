eb624b88c6e15853e81c77362e475ec2dd39ec9c
Aazib Chaudhry <aazibch47@gmail.com>
2023-10-23 13:21:48 +0500

    Create useWhenClickedOutside hook

---
+++ client/src/hooks/useWhenClickedOutside.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from 'react';
+
+export default function useWhenClickedOutside(handler: () => void) {
+  const domNodeRef = useRef<HTMLDivElement>();
+
+  useEffect(() => {
+    const callback = (event: MouseEvent) => {
+      if (event.target) {
+        if (!domNodeRef.current?.contains(event.target as Node)) {
+          handler();
+        }
+      }
+    };
+
+    document.addEventListener('mousedown', callback);
+
+    return () => {
+      document.removeEventListener('mousedown', callback);
+    };
+  }, []);
+
+  return domNodeRef;
+}
