9a9fc160534b2a33861f1369e9834f9a81b4eb97
Aazib Chaudhry <aazibch47@gmail.com>
2023-12-18 10:04:56 +0500

    Refactor DialogModal

--- client/src/components/UI/Modals/DialogModal.tsx
+++ client/src/components/UI/Modals/DialogModal.tsx
@@ -1,6 +1,6 @@
 import { IoClose } from 'react-icons/io5';
 import Button from '../Button';
-import useWhenClickedOutside from '../../../hooks/useWhenClickedOutside';
+import Modal from './Modal';
 // import Backdrop from './Backdrop';
 
 const DialogModal = ({
@@ -16,51 +16,33 @@ const DialogModal = ({
   dismissHandler: () => void;
   confirmHandler: () => void;
 }) => {
-  const modalContentRef = useWhenClickedOutside(dismissHandler);
-
   return (
-    <>
-      {/* // <!-- Main modal --> */}
-      <div className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-10 justify-center items-center w-full inset-0 max-h-full bg-black/70">
-        <div className="relative p-4 w-full max-w-2xl max-h-full mx-auto mt-32">
-          {/* <!-- Modal content --> */}
-          <div
-            ref={modalContentRef}
-            className="relative bg-white rounded-lg shadow z-20"
-          >
-            {/* <!-- Modal header --> */}
-            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t ">
-              <h3 className="text-xl font-semibold ">{heading}</h3>
-              <Button
-                iconButton
-                onClick={dismissHandler}
-                className="text-lg s-auto justify-center items-center"
-              >
-                <IoClose />
-              </Button>
-            </div>
-            {/* <!-- Modal body --> */}
-            <div className="p-4 md:p-5 space-y-4">
-              <p className="text-base leading-relaxed text-gray-500 ">
-                {textBody}
-              </p>
-            </div>
-            {/* <!-- Modal footer --> */}
-            <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b ">
-              <Button
-                styleType="primary"
-                onClick={confirmHandler}
-                className="mr-1"
-                disabled={isLoading}
-              >
-                Confirm
-              </Button>
-              <Button onClick={dismissHandler}>Cancel</Button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+    <Modal.Body dismissHandler={dismissHandler}>
+      <Modal.Header>
+        <h3 className="text-xl font-semibold ">{heading}</h3>
+        <Button
+          iconButton
+          onClick={dismissHandler}
+          className="text-lg s-auto justify-center items-center"
+        >
+          <IoClose />
+        </Button>
+      </Modal.Header>
+      <Modal.Content>
+        <p className="text-base leading-relaxed text-gray-500 ">{textBody}</p>
+      </Modal.Content>
+      <Modal.Footer>
+        <Button
+          styleType="primary"
+          onClick={confirmHandler}
+          className="mr-1"
+          disabled={isLoading}
+        >
+          Confirm
+        </Button>
+        <Button onClick={dismissHandler}>Cancel</Button>
+      </Modal.Footer>
+    </Modal.Body>
   );
 };
 
---
+++ client/src/components/UI/Modals/Modal.tsx
@@ -0,0 +1,53 @@
+import useWhenClickedOutside from '../../../hooks/useWhenClickedOutside';
+
+interface ModalBodyProps {
+  children: React.ReactNode;
+  dismissHandler: () => void;
+}
+
+const Body = ({ children, dismissHandler }: ModalBodyProps) => {
+  const modalBodyRef = useWhenClickedOutside(dismissHandler);
+
+  return (
+    <div
+      ref={modalBodyRef}
+      className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-10 justify-center items-center w-full inset-0 max-h-full bg-black/70"
+    >
+      <div className="relative p-4 w-full max-w-2xl max-h-full mx-auto mt-32">
+        {/* <!-- Modal content --> */}
+        <div className="relative bg-white rounded-lg shadow z-20">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Header = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t ">
+      {children}
+    </div>
+  );
+};
+
+const Content = ({ children }: { children: React.ReactNode }) => {
+  return <div className="p-4 md:p-5 space-y-4">{children}</div>;
+};
+
+const Footer = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b ">
+      {children}
+    </div>
+  );
+};
+
+const Modal = {
+  Body,
+  Header,
+  Content,
+  Footer
+};
+
+export default Modal;
