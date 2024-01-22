538b70d1c4c3065e3962c1a9b724591fa32a6302
Aazib Chaudhry <aazibch47@gmail.com>
2024-01-22 12:52:57 +0500

    Add back button to SettingsSidebarHeader

--- client/src/components/Settings/SettingsSidebar/SettingsSidebar.tsx
+++ client/src/components/Settings/SettingsSidebar/SettingsSidebar.tsx
@@ -3,7 +3,7 @@ import SettingsSidebarHeader from './SettingsSidebarHeader';
 
 const SettingsSidebar = () => {
   return (
-    <div className="grow-0 shrink-0 border-r border-gray-300 basis-[20rem] h-full flex-col border">
+    <div className="grow-0 shrink-0 border-r border-gray-300 basis-[25rem] h-full flex-col border">
       <SettingsSidebarHeader />
       <SettingsNav />
     </div>
--- client/src/components/Settings/SettingsSidebar/SettingsSidebarHeader.tsx
+++ client/src/components/Settings/SettingsSidebar/SettingsSidebarHeader.tsx
@@ -1,6 +1,19 @@
+import { IoIosArrowBack } from 'react-icons/io';
+import Button from '../../UI/Button';
+import { useNavigate } from 'react-router-dom';
+
 const SettingsSidebarHeader = () => {
+  const navigate = useNavigate();
+
+  const backButtonClickHandler = () => {
+    navigate(-1);
+  };
+
   return (
     <div className="border-b border-gray-300 flex items-center px-4 h-14">
+      <Button onClick={backButtonClickHandler} iconButton className="mr-5">
+        <IoIosArrowBack size="1.25em" />
+      </Button>
       <h2 className="font-semibold">Settings</h2>
     </div>
   );
