5ec6c0185d9bd2da3fc0824e588da726c65bd911
Aazib Chaudhry <aazibch47@gmail.com>
2024-01-24 12:50:55 +0500

    Modify SettingsSidebar

--- client/src/components/Settings/SettingsSidebar/SettingsSidebar.tsx
+++ client/src/components/Settings/SettingsSidebar/SettingsSidebar.tsx
@@ -1,30 +1,13 @@
-import { useMatch } from 'react-router-dom';
 import SettingsNav from './SettingsNav/SettingsNav';
 import SettingsSidebarHeader from './SettingsSidebarHeader';
+import SidebarContainer from '../../UI/SidebarContainer';
 
 const SettingsSidebar = () => {
-  const sidebarClasses = [
-    'grow',
-    'shrink-0',
-    'border-r',
-    'border-gray-300',
-    'h-full',
-    'flex-col',
-    'border',
-    'md:basis-[25rem]',
-    'md:grow-0'
-  ];
-
-  if (useMatch('/settings/*') && !useMatch('/settings/')) {
-    sidebarClasses.push('hidden');
-    sidebarClasses.push('md:block');
-  }
-
   return (
-    <div className={sidebarClasses.join(' ')}>
+    <SidebarContainer routeHiddenOn="/settings/*">
       <SettingsSidebarHeader />
       <SettingsNav />
-    </div>
+    </SidebarContainer>
   );
 };
 
---
+++ client/src/components/UI/MainContentContainer.tsx
@@ -0,0 +1,3 @@
+const MainContentContainer = () => {};
+
+export default MainContentContainer;
