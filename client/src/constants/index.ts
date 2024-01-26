78b2875cffa7381e9dee4c1e89c40bf6189dc1eb
Aazib Chaudhry <aazibch47@gmail.com>
2024-01-26 19:45:47 +0500

    Update server url for production

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -2,10 +2,10 @@ let apiUrl = 'http://localhost:8080';
 let socketUrl = 'ws://localhost:8080';
 let clientUrl = 'http://localhost:5173';
 
-// if (import.meta.env.VITE_NODE_ENV !== 'development') {
-//   apiUrl = 'api.domain.com';
-//   socketUrl = 'wss://api.domain.com'
-//   clientUrl = 'domain.com';
-// }
+if (import.meta.env.VITE_NODE_ENV !== 'development') {
+  apiUrl = 'https://zephy-messenger-api.onrender.com';
+  socketUrl = 'wss://zephy-messenger-api.onrender.com';
+  clientUrl = 'domain.com';
+}
 
 export { apiUrl, clientUrl, socketUrl };
