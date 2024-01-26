1610549c8077b28034a70accba0bf11f129ac0da
Aazib Chaudhry <aazibch47@gmail.com>
2024-01-26 20:55:40 +0500

    Update constants file

--- server/src/constants/index.ts
+++ server/src/constants/index.ts
@@ -1,1 +1,7 @@
-export const clientUrl = 'https://zephy-messenger.onrender.com';
+let clientUrl = 'http://localhost:5173';
+
+if (process.env.NODE_ENV !== 'development') {
+  clientUrl = 'https://zephy-messenger.onrender.com';
+}
+
+export { clientUrl };
