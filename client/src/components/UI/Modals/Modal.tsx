b6a4da86aa6cac2809907a10dc03def16837bd51
Aazib Chaudhry <aazibch47@gmail.com>
2024-01-27 21:16:26 +0500

    Adjust padding for non-image modals

--- client/src/components/UI/Modals/Modal.tsx
+++ client/src/components/UI/Modals/Modal.tsx
@@ -13,7 +13,7 @@ const Body = ({ children, dismissHandler }: ModalBodyProps) => {
       ref={modalBodyRef}
       className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-10 justify-center items-center w-full inset-0 max-h-full bg-black/70"
     >
-      <div className="relative p-4 w-full max-w-2xl max-h-full mx-auto mt-32">
+      <div className="relative p-2 w-full max-w-2xl max-h-full mx-auto mt-32">
         {/* <!-- Modal content --> */}
         <div className="relative bg-white rounded-lg shadow z-20">
           {children}
